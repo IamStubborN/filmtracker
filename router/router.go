c5aa90116add1bf839b8cbe06d04c264fb907990
iamstubborn <istubb@me.com>
2019-03-30 11:07:48 +0200

    CORS added.

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/IamStubborN/filmtracker/middlewares"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,7 @@ func CreateRouter() *gin.Engine {
 	router.Use(
 		gin.Recovery(),
 		middlewares.XSSMiddle(),
+		cors.Default(),
 	)
 	AddV1RouterGroup(router)
 	AddAuthRouterGroup(router)
